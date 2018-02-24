853388d6f7220e9112d2d83053650aaabf983a92
Javed Mohamed <jmohamed001@gmail.com>
2018-02-24 16:18:46 -0600

    Create ray class

---
+++ src/geom/ray.hpp
@@ -0,0 +1,20 @@
+#ifndef RAY_H_
+#define RAY_H_
+
+#include <glm/glm.hpp>
+
+namespace geom {
+  class ray {
+  public:
+    ray();
+    ray(const glm::vec3 origin_, const glm::vec3 direction_);
+    glm::vec3 origin() const;
+    glm::vec3 direction() const;
+    glm::vec3 pointat(float t) const;
+  private:
+    glm::vec3 origin_, direction_;
+  };
+}
+
+
+#endif
